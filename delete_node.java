05309d572a53aff72a8060b3b2dd0c69f15252c4
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2024-08-08 18:19:37 +0530

    Update delete_node.java

--- delete_node.java
+++ delete_node.java
@@ -6,6 +6,7 @@ import java.util.*;
       ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
+    
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
  
