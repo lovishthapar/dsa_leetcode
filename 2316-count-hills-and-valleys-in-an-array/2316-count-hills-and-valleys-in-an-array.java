da16786270cdabbb1d308425cb67876ccfcd55c8
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-07-27 11:01:45 +0530

    Time: 0 ms (100%), Space: 41.5 MB (81.97%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,72 +1,17 @@
 class Solution {
-    public int countHillValley(int[] n)
-     {
-        int le=n.length;
-        
-        int to=0;
-        int lv=0;
-        for(int i=1;i<le-1;i++)
-      {
-         int de=rc(n,i);
-         int fe=lc(n,i);
-         if(de==0 || fe==0)
-         continue;
+    public int countHillValley(int[] nums) {
+        int count = 0;
+        int left = 0;
 
-         else if(de==1 && fe==2)
-         continue;
-         
-         
-         else if(de==2 && fe==1)
-         continue;
-           
-           
-           else
-           {
-            if(n[i]!=lv)
-             to++;
-           lv=n[i];
-           }
-
-       }
-       return to;
-        
-    }
-    public int rc(int n[],int i)
-    { 
-        for(int g=i+1;g<n.length;g++)
-        {
-            if(n[g]!=n[i])
-            {
-                if(n[g]>n[i])
-                return 1;
-                if(n[g]<n[i])
-                return 2; 
-
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) || 
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    count++;
+                }
+                left = i;
             }
-
-
         }
-      return 0;
-
-
-    }
-    public int lc(int n[],int i)
-    {
-        for(int g=i-1;g>=0;g--)
-        {
-            if(n[g]!=n[i])
-            {
-                if(n[g]>n[i])
-                return 1;
-                if(n[g]<n[i])
-                return 2; 
-
-            }
-
-
-        }
-      return 0;
-
-
+        return count;
     }
 }
