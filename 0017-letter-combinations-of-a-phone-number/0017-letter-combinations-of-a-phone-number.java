1451d7f765c53513f886e1537debeec9f77d19e4
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-09-13 15:15:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (65.27%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> ae = new ArrayList<>();
+
+    public List<String> letterCombinations(String d) {
+        ae.clear(); // clear previous results
+        if (d == null || d.length() == 0) return ae;
+
+        int n = d.length();
+        String a[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        int di[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            di[i] = (
+                int)d.charAt(i) - 48; 
+        }
+
+        yu(0, di, new StringBuilder(), a);
+        return ae;
+    }
+
+    public void yu(int ci, int d[], StringBuilder e, String a[]) {
+        if (ci == d.length) {
+            ae.add(e.toString());
+            return;
+        }
+
+        for (int i = 0; i < a[d[ci]].length(); i++) {
+            e.append(a[d[ci]].charAt(i));     
+            yu(ci + 1, d, e, a);            
+            e.deleteCharAt(e.length() - 1);   
+        }
+    }
+}
