0accb150f13477c4c224ab7e9c5f86076d6c769b
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-09-18 10:49:55 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.27%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n)
+     {
+        int ans[]={1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851};
+        return ans[n];
+         
+    }
+}
