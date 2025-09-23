46f1b85dffb61cd0bd5fc889ed8cde0a178f5b1f
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-09-23 05:54:39 +0530

    Time: 1 ms (76.03%), Space: 41.4 MB (83.19%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compareVersion(String v1, String v2) {
+        String a1[] = v1.split("\\.");
+        String a2[] = v2.split("\\.");
+        int l1 = a1.length;
+        int l2 = a2.length;
+        int n = Math.max(l1, l2); // FIX 1
+
+        for (int i = 0; i < n; i++) {
+            int val1 = 0, val2 = 0;
+
+            if (i < l1) {  // FIX 2
+                int le1 = a1[i].length();
+                for (int f = 0; f < le1; f++) {
+                    int de = ((int)(a1[i].charAt(f))) - 48;
+                    val1 = val1 * 10 + de;
+                }
+            }
+
+            if (i < l2) {  // FIX 2
+                int le2 = a2[i].length();
+                for (int g = 0; g < le2; g++) {
+                    int de = ((int)(a2[i].charAt(g))) - 48;
+                    val2 = val2 * 10 + de;
+                }
+            }
+
+            if (val1 > val2) return 1;
+            if (val2 > val1) return -1;
+        }
+
+        return 0;
+    }
+}
