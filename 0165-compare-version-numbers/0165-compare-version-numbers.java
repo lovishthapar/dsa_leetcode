bf6c0468401c0acbfc9eb57c0551e588d46baefb
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-09-23 06:03:09 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -4,12 +4,12 @@ class Solution {
         String a2[] = v2.split("\\.");
         int l1 = a1.length;
         int l2 = a2.length;
-        int n = Math.max(l1, l2); // FIX 1
+        int n = Math.max(l1, l2);
 
         for (int i = 0; i < n; i++) {
             int val1 = 0, val2 = 0;
 
-            if (i < l1) {  // FIX 2
+            if (i < l1) {  
                 int le1 = a1[i].length();
                 for (int f = 0; f < le1; f++) {
                     int de = ((int)(a1[i].charAt(f))) - 48;
@@ -17,7 +17,7 @@ class Solution {
                 }
             }
 
-            if (i < l2) {  // FIX 2
+            if (i < l2) {  
                 int le2 = a2[i].length();
                 for (int g = 0; g < le2; g++) {
                     int de = ((int)(a2[i].charAt(g))) - 48;
