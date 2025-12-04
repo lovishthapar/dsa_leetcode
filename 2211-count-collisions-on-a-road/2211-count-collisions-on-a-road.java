8ad92f898d69f2e108c77a2617cfa0b114fdabf2
lovish monga <135959714+lovishthapar@users.noreply.github.com>
2025-12-04 18:00:09 +0530

    Time: 16 ms (49.6%), Space: 47.8 MB (18.15%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -3,12 +3,17 @@ class Solution {
     {
         int noc = 0;
         int l = dir.length();
+        int k = 0, j = l - 1;
+
+        while (k < l && dir.charAt(k) == 'L') k++;
+        while (j >= 0 && dir.charAt(j) == 'R') j--;
+
 
         int ri = 0;
         int le = 0;
         int stb = 0;
 
-        for(int i = 0; i < l; i++)
+        for(int i = k; i <=j; i++)
         {
             char c = dir.charAt(i);
 
